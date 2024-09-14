9cecbadf441ab665990d52d4b1a76c8cf4487bc9
Pablo Teodoro <pabloteadoro123@gmail.com>
2024-09-14 13:49:23 -0300

    creaate querys for db
    create querys db with drizzle kit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'drizzle-kit'
+import { env } from './src/env'
+
 
 export default defineConfig({
-    schema: "./schema.ts",
+    schema: "./src/db/schema.ts",
     out: './.migrations',
     dialect: 'postgresql',
    dbCredentials: {
-    url: 'postgresql://docker:docker@localhost:5432/inorbit'
+    url: env.DATABASE_URL,
    },
 })
