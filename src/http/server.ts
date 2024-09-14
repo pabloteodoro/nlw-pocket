406ee898b3572486cbeb15020050fe8b3b0e1f94
Pablo Teodoro <pabloteadoro123@gmail.com>
2024-09-14 12:51:11 -0300

    install package and configure
    install biomejs for organization code

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,1 +1,9 @@
-console.log('Hello, world!');
+import fastify from "fastify";
+
+const app = fastify()
+
+app.listen({
+    port: 3334,
+}).then(() => {
+    console.log('Server is running on port 3334')
+})
